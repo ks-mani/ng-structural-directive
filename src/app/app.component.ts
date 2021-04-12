cf2e8edf4e45326c5ded8779aae20f97e98e738a
ks-mani <kumar.mani017@gmail.com>
2021-04-12 19:06:04 +0530

    Addition of trackBy in ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import { Component, VERSION } from "@angular/core";
 export class AppComponent {
   name = "Angular " + VERSION.major;
 
+  trackByAge(index, item) {
+    return item.age;
+  }
+
   basicDetails = [
     {
       firstName: "Andrew",
