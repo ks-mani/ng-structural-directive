a1b2aea8bf7b76bd330c2d4b944038934299f5a2
ks-mani <kumar.mani017@gmail.com>
2021-04-12 13:21:45 +0530

    dummy data for the structure directive created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  protected basicDetails = [
+    {
+      firstName: "Andrew",
+      lastName: "Garfield",
+      age: "30"
+    },
+    {
+      firstName: "Hadiqa",
+      lastName: "Kiani",
+      age: "32"
+    },
+    {
+      firstName: "Tom",
+      lastName: "Holland",
+      age: "26"
+    },
+    {
+      firstName: "Hugh",
+      lastName: "Jackman",
+      age: "44"
+    },
+    {
+      firstName: "Peter",
+      lastName: "Dinklage",
+      age: "22"
+    },
+    {
+      firstName: "Lena",
+      lastName: "Headey",
+      age: "25"
+    },
+    {
+      firstName: "Kit",
+      lastName: "Harrington",
+      age: "33"
+    },
+    {
+      firstName: "Rose",
+      lastName: "Lesley",
+      age: "29"
+    },
+    {
+      firstName: "Atif",
+      lastName: "Aslam",
+      age: "19"
+    },
+    {
+      firstName: "Abida",
+      lastName: "Parveen",
+      age: "42"
+    }
+  ];
 }
